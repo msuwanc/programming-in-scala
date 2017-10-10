ad04101f408d2e01cb0d1c799d3ebcb62d70a650
Maytee <maytee.s@dotography.com>
2017-10-10 22:44:24 +0700

    7.8 nested iteration

---
+++ src/main/scala/whileloop/WhileLoop.scala
@@ -0,0 +1,16 @@
+package whileloop
+
+// 7.2
+
+class WhileLoop {
+  def gcdLoop(x: Long, y: Long): Long = {
+    var a = x
+    var b = y
+    while (a != 0) {
+      val temp = a
+      a = b % a
+      b = temp
+    }
+    b
+  }
+}
